66fe11f60f41d067fe49bb2edb8083b08f22ff2b
L3viathan <git@l3vi.de>
2019-08-09 15:52:23 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="volkswagen",
+    version="0.1.0",
+    author="L3viathan",
+    author_email="git@l3vi.de",
+    description="Detect when your tests run in a CI and make them pass",
+    url="https://github.com/L3viathan/pyvw",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+    ],
+)
